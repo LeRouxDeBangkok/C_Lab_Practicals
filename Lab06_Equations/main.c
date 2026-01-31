5d5a271115e417b5da2d9fbb652a4554cf4cca63
LeRouxDeBangkok <romain.chalvet.champagne@gmail.com>
2026-01-31 08:56:13 +0530

    6

--- Lab06_Equations/main.c
+++ Lab06_Equations/main.c
@@ -3,41 +3,62 @@
 
 int main() {
     int choice;
-    printf("Choose an esquation from the list\n");
-    printf("1. V = u + a * t\n");
-    printf("2. S = u * t + 0.5 * a * (t * t)\n");
-    printf("3. T = 2 * a + sqrt(b + 9c)\n");
-    printf("4. H = sqrt((b * b) + (p * p))\n");
-    printf("Choose the number of one of the equation (1-4): ");
-    scanf("%d", choice);
+    printf("Choose which equation you wish to evaluate:\n");
+    printf("1. V = u + a*t\n");
+    printf("2. S = u*t + 1/2*a*t^2\n");
+    printf("3. T=2*a + sqrt(b) + 9*c\n");
+    printf("4. H = sqrt(b^2 + p^2)\n");
+    printf("Choose a number from 1-4: ");
+    scanf("%d", &choice);
 
     if (choice == 1) {
         int u, a, t, res;
-        printf("Enter value of u: ");
-        scanf("%d", u);
-        printf("Enter value of a: ");
-        scanf("%d", a);
-        printf("Enter value of t: ");
-        scanf("%d", t);
-        res = u + (a*t);
-        printf("V = %d", res);
+        printf("Enter values for u, a and t\n");
+        printf("u = ");
+        scanf("%d", &u);
+        printf("a = ");
+        scanf("%d", &a);
+        printf("t = ");
+        scanf("%d", &t);
+        res = u + (a * t);
+        printf("V = %d\n", res);
     } else if (choice == 2) {
         int u, a, t;
         float res;
-        printf("Enter value of u: ");
-        scanf("%d", u);
-        printf("Enter value of a: ");
-        scanf("%d", a);
-        printf("Enter value of t: ");
-        scanf("%d", t);
-        res = (u*t) + (0.5*a*(t*t));
-        printf("V = %f", res);
+        printf("Enter values for u, a and t\n");
+        printf("u = ");
+        scanf("%d", &u);
+        printf("a = ");
+        scanf("%d", &a);
+        printf("t = ");
+        scanf("%d", &t);
+        res = (float)((u*t) + ((0.5)*a*(t*t)));
+        printf("S = %f\n", res);
     } else if (choice == 3) {
-        todo();
+        int a, b, c;
+        float res;
+        printf("Enter values for a, b and c\n");
+        printf("a = ");
+        scanf("%d", &a);
+        printf("b = ");
+        scanf("%d", &b);
+        printf("c = ");
+        scanf("%d", &c);
+        res = (float)((2*a) + sqrt(b) + (9*c));
+        printf("T = %f\n", res);
     } else if (choice == 4) {
-        todo();
+        int b, p;
+        float res;
+        printf("Enter values for b, and p\n");
+        printf("b = ");
+        scanf("%d", &b);
+        printf("p = ");
+        scanf("%d", &p);
+        res = sqrt(((b*b) + (p*p)));
+        printf("H = %f\n", res);
     } else {
-        printf("Error, invalid number in choice");
+        printf("Error, input doesn't correspond to any operation!");
+        return 1;
     }
 
     return 0;
