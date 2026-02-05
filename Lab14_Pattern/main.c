95cd61ed009482f7282468c5627d33bb5de06ec9
LeRouxDeBangkok <romain.chalvet.champagne@gmail.com>
2026-02-05 14:08:41 +0530

    fix14

--- Lab14_Pattern/main.c
+++ Lab14_Pattern/main.c
@@ -3,9 +3,9 @@
 int main() {
     int layers, diff, rem;
     printf("Number of layers: ");
-    scanf("%d", layers);
+    scanf("%d", &layers);
 
-    for (int i = 0; i < layers; i++) {
+    for (int i = 0; i < layers + 1; i++) {
         diff = layers - i;
         rem = layers - diff;
         for (int j = 0; j < diff; j++) {
@@ -14,6 +14,19 @@ int main() {
         for (int k = 0; k < rem; k++) {
             printf("*");
         }
+        printf("\n");
+    }
+
+    for (int i2 = 0; i2 < layers + 1; i2++) {
+        diff = layers - i2;
+        rem = layers - diff;
+        for (int k2 = 0; k2 < rem; k2++) {
+            printf("*");
+        }
+        for (int j2 = 0; j2 < diff; j2++) {
+            printf(" ");
+        }
+        printf("\n");
     }
 
     return 0;
