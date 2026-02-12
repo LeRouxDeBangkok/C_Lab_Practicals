14676477c493229a13307a7f27be4e3a542a50b1
LeRouxDeBangkok <romain.chalvet.champagne@gmail.com>
2026-02-12 14:49:53 +0530

    25

--- Lab25_MenuStrings/main.c
+++ Lab25_MenuStrings/main.c
@@ -12,19 +12,75 @@ int main() {
         if (choice == 1) {
             char input[100];
             printf("Enter a string: ");
-            scanf("%s", input);
+            scanf("%s[^\n]", input);
             int len = strlen(input);
-            printf("The length of your string is: %d\n", input);
+            printf("The length of your string is: %d\n", len);
+
         } else if (choice == 2) {
-            todo();
+            char input1[100];
+            char input2[100];
+            printf("Enter your first string: ");
+            scanf("%s[^\n]", input1);
+            printf("Enter your second string: ");
+            scanf("%s[^\n]", input2);
+            strcat(input1, input2);
+            printf("Printing your first string after strcat:\n");
+            printf("%s", input1);
+
         } else if (choice == 3) {
-            todo();
+            char input[100];
+            char copy[100];
+            printf("Enter a string: ");
+            scanf("%s[^\n]", input);
+            strcat(copy, input);
+            printf("Here is the copy of your string stored in another variable:\n");
+            printf("%s", copy);
+
         } else if (choice == 4) {
-            todo();
+            int res = 0;
+            char input1[100];
+            char input2[100];
+            printf("Enter your first string: ");
+            scanf("%s[^\n]", input1);
+            printf("Enter your second string: ");
+            scanf("%s[^\n]", input2);
+            res = strcmp(input1, input2);
+            if (res == 0) {
+                printf("Both strings are equal\n");
+            } else if (res > 0) {
+                printf("String1 > String2\n");
+            } else {
+                printf("String2 > String1\n");
+            }
+
         } else if (choice == 5) {
-            todo();
+            char input[100];
+            printf("Enter your string: ");
+            scanf("%s[^\n]", input);
+            /*
+            strrev(input);
+            printf("After strrev: %s", input);
+
+            The code above does not work, reason:
+            Using GCC to compile, even with the string.h library, strrev in
+            not in the GCC compiler, so this does not compile if using GCC.
+            I've provided my own implmentation of strrev below as a replacement
+            */
+
+            int i = 0;
+            int j = strlen(input) - 1;
+            while (i < j) {
+                char temp = input[i];
+                input[i] = input[j];
+                input[j] = temp;
+                i++;
+                j--;
+            }
+            printf("After strrev: %s\n", input);
+
         } else {
             printf("Error, invalid choice\n");
+            return 1;
         }
     }
     return 0;
